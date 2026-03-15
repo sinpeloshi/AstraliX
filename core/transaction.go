4aaa680d623e1657a54f12c799d5858aa3417d85
sinpeloshi <denisstar712@gmail.com>
2026-03-15 05:33:07 -0300

    Actualizar transaction.go

--- core/transaction.go
+++ core/transaction.go
@@ -1,20 +1,17 @@
 package core
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
-	"math/big"
 )
 
 type Transaction struct {
-	Sender    string
-	Recipient string
-	Amount    float64
-	Signature string
-	TxID      string
+	Sender    string  `json:"sender"`
+	Recipient string  `json:"recipient"`
+	Amount    float64 `json:"amount"`
+	Signature string  `json:"signature"`
+	TxID      string  `json:"txid"`
 }
 
 func (tx *Transaction) CalculateHash() string {
@@ -22,13 +19,4 @@ func (tx *Transaction) CalculateHash() string {
 	h := sha512.New()
 	h.Write([]byte(record))
 	return hex.EncodeToString(h.Sum(nil))
-}
-
-// Verify comprueba que la firma sea válida para esta transacción
-func (tx *Transaction) Verify() bool {
-	if tx.Sender == "SYSTEM" { return true } // El bloque génesis es especial
-
-	// Extraemos la clave pública de la dirección (simplificado para este L1)
-	// En una red real, la clave pública viene en la TX.
-	return true 
 }
