f70a651ada9655cd6d9bb83dbc9bba18528ab70f
sinpeloshi <denisstar712@gmail.com>
2026-03-15 04:44:46 -0300

    Actualizar wallet.go

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -4,35 +4,31 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 )
 
-// Wallet almacena tu clave privada (secreta) y tu clave pública
 type Wallet struct {
 	PrivateKey *ecdsa.PrivateKey
 	PublicKey  []byte
 }
 
-// NewWallet usa criptografía de curva elíptica (igual que Bitcoin) para crear tus llaves
 func NewWallet() *Wallet {
-	// Generamos la clave privada matemática
-	private, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	// ¡Upgrade a la curva P-521 (Nivel Máximo del NIST)!
+	private, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
 	if err != nil {
 		panic(err)
 	}
 	
-	// Derivamos la clave pública a partir de la privada
 	pubKey := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
 	return &Wallet{private, pubKey}
 }
 
-// GetAddress convierte tu clave pública en una dirección legible para la red
 func (w *Wallet) GetAddress() string {
-	// Hasheamos la clave pública por seguridad
-	pubKeyHash := sha256.Sum256(w.PublicKey)
+	// Hasheamos la pública con SHA-512
+	pubKeyHash := sha512.Sum512(w.PublicKey)
 	
-	// Creamos tu dirección única agregando el prefijo "AX" de AstraliX
-	address := "AX" + hex.EncodeToString(pubKeyHash[:])[:38]
+	// Generamos la nueva dirección AstraliX ultra-segura (64 caracteres)
+	address := "AX" + hex.EncodeToString(pubKeyHash[:])[:64]
 	return address
 }
