34cc3b51d1dd07fa85e105900cec59ae03ba9d02
sinpeloshi <denisstar712@gmail.com>
2026-03-15 05:24:18 -0300

    Actualizar block.go

--- core/block.go
+++ core/block.go
@@ -9,7 +9,7 @@ import (
 type Block struct {
 	Index        int64
 	Timestamp    int64
-	Transactions []Transaction 
+	Transactions []Transaction // Usamos el tipo Transaction definido en el otro archivo
 	PrevHash     string
 	Hash         string
 	Nonce        int
@@ -17,6 +17,7 @@ type Block struct {
 }
 
 func (b *Block) CalculateHash() string {
+	// Convertimos las transacciones a texto para incluirlas en el Hash
 	txData := fmt.Sprintf("%v", b.Transactions)
 	record := fmt.Sprintf("%d%d%s%s%d", b.Index, b.Timestamp, txData, b.PrevHash, b.Nonce)
 	h := sha512.New()
@@ -25,9 +26,10 @@ func (b *Block) CalculateHash() string {
 }
 
 func (b *Block) Mine() {
-	// Creamos el prefijo de ceros según la dificultad
-	target := strings.Repeat("0", b.Difficulty)
-
+	target := string(make([]byte, b.Difficulty))
+	for i := 0; i < b.Difficulty; i++ {
+		target = target[:i] + "0" + target[i+1:]
+	}
 	for {
 		b.Hash = b.CalculateHash()
 		if b.Hash[:b.Difficulty] == target {
@@ -36,6 +38,3 @@ func (b *Block) Mine() {
 		b.Nonce++
 	}
 }
-
-// Necesitamos importar "strings" para el Repeat
-import "strings"
